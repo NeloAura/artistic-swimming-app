8aa3560e3b39f856343232ba7831dbffda145743
NeloAura <1132725@student.uoc.cw>
2023-03-16 13:33:38 -0400

    config changes  for horspot

--- src/server/server.mjs
+++ src/server/server.mjs
@@ -2,12 +2,15 @@
 import http from 'http';
 import express from 'express';
 import cors from 'cors';
-import {exec} from 'child_process'
-import hotspot from  'node-hotspot'
+// import {exec} from 'child_process'
+import hotspot from 'node-hotspot'
+
 
 //constants here
 const Api = express();
 const HTTP = http.Server(Api);
+
+
 // const command = '"C:\\Program Files (x86)\\Treexy\\Omnify Hotspot\\OmnifyHotspot.exe"';
 // const args = ['start', 'Artistic Swimming', 'notinpan10'];
 
@@ -25,37 +28,47 @@ Api.use(cors());
 
 Api.get('/test', (req, res) => res.status(200).send('success!'));
 
-
-
-
-
-exec('netsh wlan set hostednetwork mode=allow ssid=Artistic key=notinpan10', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-
-  // The command executed successfully, so we can now start the hosted network
-  exec('netsh wlan start hostednetwork', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+HTTP.listen(3001, () => {
+  console.log('listening on *:3001');
+  
 });
 
 
-// hotspot.enable(opts)
-//     .then(function() {
-//         console.log('Hotspot Enabled')
-//     })
-//     .catch(function(e) {
-//         Console.log('Something went wrong; Perms?', e)
-//     });
 
-//     hotspot.stats(opts)
-//     .then(function(status) {
-//         console.log('Hotspot status: ' + status) //status contains clients object and state
-//     });    
+// exec('netsh wlan set hostednetwork mode=allow ssid=Artistic key=notinpan10', (error, stdout, stderr) => {
+//   if (error) {
+//     console.error(`exec error: ${error}`);
+//     return;
+//   }
+
+//   // The command executed successfully, so we can now start the hosted network
+//   exec('netsh wlan start hostednetwork', (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`);
+//       return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//   });
+// });
+
+
+
+const options = {
+  ssid: 'Aura', // Replace with your desired network name
+  password: 'notinpan10', // Replace with your desired password
+  maxClients: 8, // Maximum number of clients allowed to connect,
+  force: true,
+  disableInternet: false // Set to true to disable internet access for connected devices
+};
+
+
+hotspot.enable(options)
+  .then(() => console.log('Hotspot started successfully!'))
+  .catch(error => console.error('Failed to start hotspot:', error));
+
+
+  // hotspot.disable()
+  // .then(() => console.log('Hotspot stopped successfully!'))
+  // .catch(error => console.error('Failed to stop hotspot:', error));
+   
