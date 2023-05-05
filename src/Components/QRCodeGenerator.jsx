fd8ff421084fa845b9d58b6311772ad9d0474709
NeloAura <1132725@student.uoc.cw>
2023-05-05 14:30:27 -0400

    qr-code

--- src/Components/QRCodeGenerator.jsx
+++ src/Components/QRCodeGenerator.jsx
@@ -2,12 +2,53 @@ import { Image,Box, Center, Heading } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import qrcode from "qrcode";
 
-export function QRCodeGenerator({ ssid, password }) {
+function getIpAddress() {
+  return new Promise((resolve, reject) => {
+    const ifaces = os.networkInterfaces();
+    let ipAddress;
+
+    Object.keys(ifaces).forEach(ifname => {
+      ifaces[ifname].forEach(iface => {
+        if (iface.family === 'IPv4' && !iface.internal) {
+          ipAddress = iface.address;
+        }
+      });
+    });
+
+    if (ipAddress) {
+      resolve(ipAddress);
+    } else {
+      reject(new Error('Unable to retrieve IP address'));
+    }
+  });
+}
+
+function generateSecretCode(length = 8) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let code = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters[randomIndex];
+  }
+
+  return code;
+}
+
+
+
+
+
+
+export async function QRCodeGenerator({ ssid, password }) {
   const [qrCodeDataURL, setQRCodeDataURL] = useState("");
-  //
+  const [ipAddress] = useState(generateSecretCode());
+  const [secretCode] = useState(await getIpAddress());
+  
 
   useEffect(() => {
-    const wifiQRCodeData = `WIFI:S:${ssid};T:WPA;P:${password};;`;
+    const wifiQRCodeData = `WIFI:S:${ssid};T:WPA;P:${password};;T:IP;P:${ipAddress};;T:SECRET;P:${secretCode};;`;
     qrcode.toDataURL(wifiQRCodeData).then((dataURL) => {
       
       setQRCodeDataURL(dataURL);
